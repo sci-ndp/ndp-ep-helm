1978883a4ff6ef85dccf636978a2b7877d7ca872
Yutian Qin <yutianqinx@gmail.com>
2026-04-22 22:51:32 -0600

    minor tweak

--- helm.local.sh
+++ helm.local.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Deploy scidx-helm.
+# Deploy ndp-ep-helm.
 #
 # Fetches the federation document before calling helm so that optional
 # sub-charts (kafka-kraft, ndp-jupyterhub) can be enabled/disabled based
